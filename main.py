ee0bebfc81c9164c9341bc600b6958ea15bc561a
Natan Santos <72272206+Natan-TI@users.noreply.github.com>
2023-09-27 12:57:24 -0300

    update comments

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 import cv2
 import dlib
 
-# Carregar o detector de faces do dlib
+# Load dlib face detector
 detector = dlib.get_frontal_face_detector()
 
-# Inicializar a captura de vídeo da webcam
+# Start webcan video
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Capturar um quadro da webcam
+    # Capture a webcam frame
     ret, frame = cap.read()
 
     if not ret:
         break
 
-    # Converter o quadro para escala de cinza
+    # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detectar faces no quadro
+    # Detect faces in the frame
     faces = detector(gray)
 
-    # Contar o número de faces detectadas
+    # Count the number of faces detected
     num_faces = len(faces)
 
-    # Desenhar retângulos ao redor das faces detectadas
+    # Draw rectangles around detected faces
     for face in faces:
         x, y, w, h = face.left(), face.top(), face.width(), face.height()
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # Exibir o número de pessoas detectadas
+    # View the number of people detected
     cv2.putText(frame, f'Pessoas: {num_faces}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    # Exibir o quadro resultante
+    # View the resulting frame
     cv2.imshow('Detecção Facial', frame)
 
-    # Verificar se o usuário pressionou a tecla 'q' para sair
+    # Check if the user pressed the 'q' key to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Liberar a captura de vídeo e fechar a janela
+# Release video capture and close the window
 cap.release()
 cv2.destroyAllWindows()
